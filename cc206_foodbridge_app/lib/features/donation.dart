f5d12d5c4d192a787afbce7c79e4a9854b1291b3
xymontroy <95668117+xymontroy@users.noreply.github.com>
2023-10-04 15:58:10 +0800

    Eligido_branches
    with add button

--- cc206_foodbridge_app/lib/features/donation.dart
+++ cc206_foodbridge_app/lib/features/donation.dart
@@ -1,41 +1,75 @@
 import 'package:flutter/material.dart';
 
-class Donation extends StatelessWidget{
+class Donation extends StatelessWidget {
   const Donation({super.key});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
-        Image.network("https://tse3.mm.bing.net/th?id=OIP.xgn6bhRgPCVyXMiXh16bXAAAAA&pid=Api", 
-         width: 200,
+          Image.network(
+            "https://tse3.mm.bing.net/th?id=OIP.xgn6bhRgPCVyXMiXh16bXAAAAA&pid=Api",
+            width: 200,
             height: 200,
-        ),
-        Row(mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(
-            Icons.favorite,
-            color: Colors.red,
-            size: 32,
           ),
-          Text(
-            'Guava Pineapple Juice',
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold, 
-            )
-          )
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.favorite,
+                color: Colors.red,
+                size: 32,
+              ),
+              Text(
+                'Guava Pineapple Juice',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                icon: Icon(
+                  Icons.add,
+                  size: 32,
+                  color: Colors.blue,
+                ),
+                onPressed: () {
+                  //actions
+                },
+              ),
+              Text(
+                '100',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              IconButton(
+                icon: Icon(
+                  Icons.remove,
+                  size: 32,
+                  color: Colors.blue,
+                ),
+                onPressed: () {
+                  //actions
+                },
+              ),
+            ],
+          ),
         ],
-        ),
-        ],
-        ),
-      );
+      ),
+    );
   }
-  
-  void main(){
+
+  void main() {
     runApp(MaterialApp(
-    home: Donation(),
+      home: Donation(),
     ));
   }
 }
