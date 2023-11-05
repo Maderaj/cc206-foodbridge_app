6960cee0e17c803f7926c850ef8c685034489435
Maderaj <145520236+Maderaj@users.noreply.github.com>
2023-11-05 18:20:18 +0800

    updated_madera

--- cc206_foodbridge_app/lib/features/donation.dart
+++ cc206_foodbridge_app/lib/features/donation.dart
@@ -1,85 +1,128 @@
 import 'package:flutter/material.dart';
 
 class Donation extends StatelessWidget {
-  const Donation({super.key});
+  const Donation({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Column(
-          children: [
-            Image.network(
-              "https://tse3.mm.bing.net/th?id=OIP.xgn6bhRgPCVyXMiXh16bXAAAAA&pid=Api",
-              width: 200,
-              height: 200,
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 5, 230, 230),
+          centerTitle: true,
+          title: Text(
+            'Donation',
+            style: TextStyle(color: Colors.black),
+          ),
+          leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back,
+              color: Colors.black,
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-               
-                Text(
-                  'Guava Pineapple Juice',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-               Icon(
+            onPressed: () {
+              // Handle back button press
+            },
+          ),
+        ),
+        body: Stack(
+          children: [
+            Align(
+              alignment: Alignment.topCenter,
+              child: Image.network(
+                "https://myafricacaribbean.com/cdn/shop/products/image_1080x_af80e0b6-1c57-4d26-855a-1955c0d0c00b.png?v=1630235570",
+                fit: BoxFit.cover,
+                width: 500,
+                height: 300,
+              ),
+            ),
+            Positioned(
+              top: 250,
+              right: 22,
+              child: IconButton(
+                icon: Icon(
                   Icons.favorite,
                   color: Colors.red,
                   size: 32,
                 ),
-                ],
-            ),
-            Text(
-              'True Juice, a combination of natural guava pineapple Juice.',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.normal,
+                onPressed: () {
+                  // Handle button tap here
+                },
               ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+            Column(
               children: [
-                IconButton(
-                  icon: Icon(
-                    Icons.remove,
-                    size: 25,
-                    color: Colors.black,
-                  ),
-                  onPressed: () {},
+                SizedBox(height: 310),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Guava Pineapple Juice',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
                 ),
+                SizedBox(height: 15),
                 Text(
-                  '100',
+                  'True Juice, a combination of natural guava pineapple Juice.',
                   style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                    fontWeight: FontWeight.normal,
                   ),
                 ),
-                IconButton(
-                  icon: Icon(
-                    Icons.add,
-                    size: 25,
-                    color: Colors.black,
-                  ),
-                  onPressed: () {},
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      icon: Icon(
+                        Icons.remove,
+                        size: 25,
+                        color: Colors.black,
+                      ),
+                      onPressed: () {
+                        // Handle button tap here
+                      },
+                    ),
+                    Text(
+                      '100',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    IconButton(
+                      icon: Icon(
+                        Icons.add,
+                        size: 25,
+                        color: Colors.black,
+                      ),
+                      onPressed: () {
+                        // Handle button tap here
+                      },
+                    ),
+                  ],
                 ),
-               ],
-               ),
-               SizedBox(height: 20),
-               ElevatedButton(
-               onPressed: () {},
-               style: ElevatedButton.styleFrom(
-
-
-                backgroundColor: Color.fromARGB(255, 5, 230, 230), 
-                fixedSize:Size(200, 50),
-              ),
-              child: Text(
-                "DONATE",
-                style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold,fontSize: 24,),
-              ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    // Handle donation button tap here
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromARGB(255, 5, 230, 230),
+                    fixedSize: Size(200, 50),
+                  ),
+                  child: Text(
+                    "DONATE",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24,
+                    ),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
@@ -87,8 +130,8 @@ class Donation extends StatelessWidget {
     );
   }
 }
-  void main(){
-    runApp(MaterialApp(
-      home: Donation(),
-    ));
-  }
+void main() {
+  runApp(MaterialApp(
+    home: Donation(),
+  ));
+}
